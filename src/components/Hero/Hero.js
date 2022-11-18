5aba9c62e773ef7b843685e0d8282c7eb8968d3d
Marcin Kala <kalamarcin1@gmail.com>
2022-11-18 19:19:30 +0100

    img

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,18 @@
 import './Hero.scss'
+import me from '../../assets/images/me.png' 
 
 const Hero = () => {
 	return (
-		<div className="hero-box">
-			<h1>Nice to meet you!</h1>
+	<>
+		<div className="photo-box">
+			<img src={me} alt="" />
 		</div>
+		<div className="hero-box">
+			<h2>Nice to meet you!</h2>
+			<h2>I'm <span>Marcin Kala</span>.</h2>
+
+		</div>
+		</>
 	)
 }
 
