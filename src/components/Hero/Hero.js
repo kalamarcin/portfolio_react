6ca7fc3baf68f23544858c91f6cabb2cc8053d48
Marcin Kala <kalamarcin1@gmail.com>
2022-11-18 19:38:07 +0100

    hero desc

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,28 @@
 import './Hero.scss'
-import me from '../../assets/images/me.png' 
+import me from '../../assets/images/me.png'
 
 const Hero = () => {
 	return (
-	<>
-		<div className="photo-box">
-			<img src={me} alt="" />
-		</div>
-		<div className="hero-box">
-			<h2>Nice to meet you!</h2>
-			<h2>I'm <span>Marcin Kala</span>.</h2>
-
-		</div>
+		<>
+			<div className="photo-box">
+				<img src={me} alt="osoba w czapce i okularach przeciwsponecznych na czarno-biało" />
+			</div>
+			<div className="hero-box">
+				<h2>Nice to meet you!</h2>
+				<h2>
+					I'm <span>Marcin Kala</span>.
+				</h2>
+				<div className="description">
+					<p>
+						For many years I was working as a freelance graphic designer, creating visual identification and DPT. From
+						an early age, I created simple HTML and CSS websites. I am currently studying at the Software Development
+						Academy. The program of the course is HTML, CSS (and SASS), JavaScript, React, Typescript. During the course
+						I got acquainted with the scrum methodology, I used GIT and I had the opportunity to learn the best
+						programming practices. I love learning new things and getting to know new concepts. Able to effectively
+						self-manage during independent projects, as well as collaborate as part of a productive team.
+					</p>
+				</div>
+			</div>
 		</>
 	)
 }
