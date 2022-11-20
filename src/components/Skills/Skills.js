f1af5ce0b179ace6d6beda2f3028dd592782df7e
Marcin Kala <kalamarcin1@gmail.com>
2022-11-20 18:28:43 +0100

    nex trans

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,33 +1,37 @@
 import './Skills.scss'
 
+//translation
+import { useTranslation } from 'react-i18next';
+
 const Skills = () => {
+	const { t } = useTranslation();
 	return (
 		<>
 			<section className="skill-section">
 				<div className="skill-grid">
 					<div className="cell">
 						<p>HTML</p>
-						<p>1 year experience</p>
+						<p>1 {t('exp.3')}</p>
 					</div>
 					<div className="cell">
 						<p>CSS</p>
-                        <p>1 year experience</p>
+                        <p>1 {t('exp.3')}</p>
 					</div>
 					<div className="cell">
 						<p>Javascript</p>
-						<p>1 year experience</p>
+						<p>1 {t('exp.3')}</p>
 					</div>
 					<div className="cell">
 						<p>React</p>
-						<p>6 months experience</p>
+						<p>6 {t('exp.2')}</p>
 					</div>
 					<div className="cell">
 						<p>Sass</p>
-						<p>1 year experience</p>
+						<p>1 {t('exp.3')}</p>
 					</div>
 					<div className="cell">
 						<p>GIT</p>
-						<p>1 year experience</p>
+						<p>1 {t('exp.3')}</p>
 					</div>
 				</div>
 			</section>
