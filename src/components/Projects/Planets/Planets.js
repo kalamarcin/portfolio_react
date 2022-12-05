9f634a4f47b524808076249ec0a51ccce03db8c8
Marcin Kala <kalamarcin1@gmail.com>
2022-12-05 11:38:26 +0100

    links update

--- src/components/Projects/Planets/Planets.js
+++ src/components/Projects/Planets/Planets.js
@@ -7,11 +7,11 @@ const Planets = props => {
       <img src={props.imgSrc} alt="" />
       <div className="fragment">
         <div className="text">
-          <a href="https://devfinder-mk.netlify.app" target="_blank" rel="noopener noreferrer" className="text__p">
+          <a href="/" target="_blank" rel="noopener noreferrer" className="text__p">
             VIEW PROJECT
           </a>
           <a
-            href="https://github.com/kalamarcin/github-user-search"
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
             className="text__p">
@@ -23,11 +23,11 @@ const Planets = props => {
     <h3 className="cell__h3">{props.name}</h3>
     <p className="cell__p">{props.tech}</p>
     <div className="hidden-box">
-      <a href="https://devfinder-mk.netlify.app/" target="_blank" rel="noopener noreferrer" className="hidden-txt">
+      <a href="/" target="_blank" rel="noopener noreferrer" className="hidden-txt">
         VIEW PROJECT
       </a>
       <a
-        href="https://github.com/kalamarcin/github-user-search"
+        href="/"
         target="_blank"
         rel="noopener noreferrer"
         className="hidden-txt">
